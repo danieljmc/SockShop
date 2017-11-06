4d17f86c0eb7c0640aba88c70b2b17e3fe0b85a9
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2017-11-06 15:27:23 +0000

    added routes for navbar pages

--- index.php
+++ index.php
@@ -6,8 +6,28 @@ $app = new \Slim\App($container);
 $container['view'] = new \Slim\Views\PhpRenderer("./html");
 
 // Root directory (ie home)
-$app->get('/', function ($request, $response, $args) {
+$app->get('/', function($request, $response, $args) {
     $response = $this->view->render($response, "home.html");
+    return $response;
+});
+
+$app->get('/about', function($request, $response, $args) {
+    $response = $this->view->render($response, "about.html");
+    return $response;
+});
+
+$app->get('/contact', function($request, $response, $args) {
+    $response = $this->view->render($response, "contact.html");
+    return $response;
+});
+
+$app->get('/stores', function($request, $response, $args) {
+    $response = $this->view->render($response, "locator.html");
+    return $response;
+});
+
+$app->get('/myaccount', function($request, $response, $args) {
+    $response = $this->view->render($response, "login.html");
     return $response;
 });
 
