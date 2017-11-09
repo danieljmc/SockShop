303b524a682499da0aedfaa0d739ffc0c09e82f4
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2017-11-09 16:56:02 +0000

    forgot to remove undefined variable

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $app = new \Slim\App($container);
 $container['view'] = new \Slim\Views\PhpRenderer("./html");
 
 // Root directory (ie home)
-$app->get($req->getRootUri() . '/', function($request, $response, $args) {
+$app->get('/', function($request, $response, $args) {
     $response = $this->view->render($response, "home.html");
     return $response;
 });
