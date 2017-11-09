98b708dee9ce246c2b300e5b534ff6eede567dd1
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2017-11-09 16:55:07 +0000

    backtracked on change for url subrouting

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ require 'vendor/autoload.php';
 $container = new \Slim\Container;
 $app = new \Slim\App($container);
 $container['view'] = new \Slim\Views\PhpRenderer("./html");
-$req = $app->request;
-$rootUri = $req->getRootUri();
-
 
 // Root directory (ie home)
 $app->get($req->getRootUri() . '/', function($request, $response, $args) {
@@ -18,12 +15,12 @@ $app->get('/search[{q}]', function ($request, $response, $args) use ($app) {
     return $_GET['q'];
 });
 
-$app->get($rootUri . '/about', function($request, $response, $args) {
+$app->get( '/about', function($request, $response, $args) {
     $response = $this->view->render($response, "about.html");
     return $response;
 });
 
-$app->get('/contact/', function($request, $response, $args) {
+$app->get('/contact', function($request, $response, $args) {
     $response = $this->view->render($response, "contact.html");
     return $response;
 });
@@ -33,7 +30,7 @@ $app->get('/stores/', function($request, $response, $args) {
     return $response;
 });
 
-$app->get('/myaccount/', function($request, $response, $args) {
+$app->get('/myaccount', function($request, $response, $args) {
     $response = $this->view->render($response, "login.html");
     return $response;
 });
