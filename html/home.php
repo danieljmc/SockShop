2dc084b1a2647feaf408be5b1c0aa56cf68ad742
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2017-11-10 14:58:36 +0000

    added footer file

--- html/home.php
+++ html/home.php
@@ -95,27 +95,7 @@
 	</div>
 
     <!-- The footer -->
-    <div class="col-md-12" style="padding-top:20px;">
-      <ul class="col-md-4" style="text-align:center; list-style-type: none;">
-        <li>Company</li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Careers</a></li>
-      </ul>
-      <ul class="col-md-4" style="text-align:center; list-style-type: none;">
-        <li>More</li>
-        <li><a href="#">Acessibility</a></li>
-        <li><a href="#">Legal</a></li>
-        <li><a href="#">Privacy</a></li>
-        <li><a href="#">Terms of Use</a></li>
-      </ul>
-      <ul class="col-md-4" style="text-align:center; list-style-type: none;">
-        <li>Login</li>
-        <li><a href="#">Staff</a></li>
-        <li><a href="#">Manufacturer</a></li>
-        <li><a href="#">Customer</a></li>
-      </ul>
-    </div>
+    <?php include('../php/footer.php')?>
 
 
   </div>
