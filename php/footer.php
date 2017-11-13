183cb3f91462988b05e5fe918da92f42a966a4bb
EduardStanev <EduardStanev@computing.dundee.ac.uk>
2017-11-13 19:33:41 +0000

    Finished footer.

--- php/footer.php
+++ php/footer.php
@@ -1,16 +1,17 @@
+<!--First part - Company, info and login links.-->
 <div class="col-md-12" style="padding-top:20px;">
 	<hr>
     <div class="col-md-4" style="text-align:center; list-style-type: none;">
 		<ul class="nav nav-pills nav-stacked">
-			<li>Company</li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
+			<li style="font-weight:bold">Company</li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
 			<li><a href="#">Careers</a></li>
         </ul>
     </div>
     <div class="col-md-4" style="text-align:center; list-style-type: none;">
 		<ul class="nav nav-pills nav-stacked">
-			<li>More</li>
+			<li style="font-weight:bold">More Information</li>
 			<li><a href="#">Accessibility</a></li>
 			<li><a href="#">Legal</a></li>
 			<li><a href="#">Privacy</a></li>
@@ -19,31 +20,22 @@
     </div>
     <div class="col-md-4" style="text-align:center; list-style-type: none;">
 		<ul class="nav nav-pills nav-stacked">
-			<li>Login</li>
-			<li><a href="#">Staff</a></li>
-			<li><a href="#">Manufacturer</a></li>
-			<li><a href="#">Customer</a></li>
+			<li style="font-weight:bold">Login Pages</li>
+			<li><a href="login.php">Customers</a></li>
+			<li><a href="stafflogin.php">Staff</a></li>
+			<li><a href="manufacturerlogin.php">Manufacturers</a></li>
 		</ul>
     </div>
 </div>
 
+<!--Second part - Social media and copyright.-->
 <div class="container">
-	<div class="row">
-		<div class="span12">
-			<div class="span8">
-				<a href="#">Accessibility /</a>  
-				<a href="#">Terms of Service /</a>    
-				<a href="#">Privacy /</a>    
-				<a href="#">Security</a>
-				<p class="muted pull-right">© 2017 SockDirect. All rights reserved.</p>
-			</div>
-		</div>
-	</div>
-	<div class="text-center center-block">
+	<div class="text-center" style="margin-left:-25px">
 		<a href="https://www.facebook.com"><i class="fa fa-facebook-square fa-3x social"></i></a>
 		<a href="https://twitter.com"><i class="fa fa-twitter-square fa-3x social"></i></a>
 		<a href="https://plus.google.com"><i class="fa fa-google-plus-square fa-3x social"></i></a>
 		<a href="mailto:sockdirect@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
+		<p class="muted clearfix center-block" style="padding-top:10px" >© 2017 SockDirect. All rights reserved.</p>
 	</div>
 	<hr />
 </div>
