1be612c6a594cd50e8aa51853f14e30bf5d73c13
EduardStanev <EduardStanev@computing.dundee.ac.uk>
2017-11-13 19:02:57 +0000

    Updated footer.

--- php/footer.php
+++ php/footer.php
@@ -1,25 +1,49 @@
-<?php
+<div class="col-md-12" style="padding-top:20px;">
+	<hr>
+    <div class="col-md-4" style="text-align:center; list-style-type: none;">
+		<ul class="nav nav-pills nav-stacked">
+			<li>Company</li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Careers</a></li>
+        </ul>
+    </div>
+    <div class="col-md-4" style="text-align:center; list-style-type: none;">
+		<ul class="nav nav-pills nav-stacked">
+			<li>More</li>
+			<li><a href="#">Accessibility</a></li>
+			<li><a href="#">Legal</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="#">Terms of Use</a></li>
+		</ul>
+    </div>
+    <div class="col-md-4" style="text-align:center; list-style-type: none;">
+		<ul class="nav nav-pills nav-stacked">
+			<li>Login</li>
+			<li><a href="#">Staff</a></li>
+			<li><a href="#">Manufacturer</a></li>
+			<li><a href="#">Customer</a></li>
+		</ul>
+    </div>
+</div>
 
-  echo "<div class=\"col-md-12\" style=\"padding-top:20px;\">
-    <ul class=\"col-md-4\" style=\"text-align:center; list-style-type: none;\">
-      <li>Company</li>
-      <li><a href=\"#\">About</a></li>
-      <li><a href=\"#\">Contact</a></li>
-      <li><a href=\"#\">Careers</a></li>
-    </ul>
-    <ul class=\"col-md-4\" style=\"text-align:center; list-style-type: none;\">
-      <li>More</li>
-      <li><a href=\"#\">Acessibility</a></li>
-      <li><a href=\"#\">Legal</a></li>
-      <li><a href=\"#\">Privacy</a></li>
-      <li><a href=\"#\">Terms of Use</a></li>
-    </ul>
-    <ul class=\"col-md-4\" style=\"text-align:center; list-style-type: none;\">
-      <li>Login</li>
-      <li><a href=\"#\">Staff</a></li>
-      <li><a href=\"#\">Manufacturer</a></li>
-      <li><a href=\"#\">Customer</a></li>
-    </ul>
-  </div>";
-
-?>
+<div class="container">
+	<div class="row">
+		<div class="span12">
+			<div class="span8">
+				<a href="#">Accessibility /</a>  
+				<a href="#">Terms of Service /</a>    
+				<a href="#">Privacy /</a>    
+				<a href="#">Security</a>
+				<p class="muted pull-right">© 2017 SockDirect. All rights reserved.</p>
+			</div>
+		</div>
+	</div>
+	<div class="text-center center-block">
+		<a href="https://www.facebook.com"><i class="fa fa-facebook-square fa-3x social"></i></a>
+		<a href="https://twitter.com"><i class="fa fa-twitter-square fa-3x social"></i></a>
+		<a href="https://plus.google.com"><i class="fa fa-google-plus-square fa-3x social"></i></a>
+		<a href="mailto:sockdirect@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
+	</div>
+	<hr />
+</div>
